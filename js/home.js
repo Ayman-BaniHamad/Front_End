e4a9a9ce84e4bd9cbe49f6db0b2b35966bee6942
Ayman-BaniHamad <agbanihamad20@cit.just.edu.jo>
2025-09-27 17:39:04 +0300

    27-Sep-2025 Lecture

---
+++ js/home.js
@@ -0,0 +1,26 @@
+var age = 20;
+console.log("Age 1 = " , age);
+var age = 30;
+console.log("Age 2 = " , age);
+let price = 100;
+console.log("Price 1 = " , price);
+price = 200;
+console.log("Price 2 = " , price);
+const tax = 0.1;
+console.log("Tax 1 = " , tax);
+/*------------------------------*/ 
+var isNew = false;
+var marks = [60 , 70 , 80 , 90 , 95];
+console.log("Marks = " , marks);
+var student = {
+name : "Ayman" , age: 23 , isNew : true
+}
+console.log("Student Information: " , student);
+if(age < 18){
+    console.log("You are not allowed to enter");
+} else {
+    console.log("You are allowed to enter");
+}
+if (age >=10 ){
+ console.log("Age is greater than 10");
+}
